1989c69ccccfaa7bfb6794ea827206cd2d6fdce5
Nguyễn Thanh Niên <nienntbc00187@fpt.edu.vn>
2024-08-29 17:49:35 +0700

    Add project files.

---
+++ Data/ApplicationUser.cs
@@ -0,0 +1,14 @@
+using Microsoft.AspNetCore.Identity;
+
+namespace BlazorApp3.Data
+{
+    // Add profile data for application users by adding properties to the ApplicationUser class
+    public class ApplicationUser : IdentityUser
+    {
+        public string Name { get; set; }
+        public string Code { get; set; }
+        public string Role { get; set; }
+
+    }
+
+}
